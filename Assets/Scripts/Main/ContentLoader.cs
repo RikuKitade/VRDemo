9b6230007088702d2f1e0ce5fe5c44fbc6e8b682
timothsp <timothsp@uci.edu>
2017-02-09 20:35:50 -0800

    Done?

--- Assets/Scripts/Main/ContentLoader.cs
+++ Assets/Scripts/Main/ContentLoader.cs
@@ -6,9 +6,13 @@ public class ContentLoader : MonoBehaviour {
 
     public List<Sprite> mainImageList = new List<Sprite>();
     public List<Sprite> leftImageList = new List<Sprite>();
+    public List<Sprite> left2ImageList = new List<Sprite>();
+    public List<Sprite> left3ImageList = new List<Sprite>();
     public List<Sprite> rightImageList = new List<Sprite>();
+    public List<Sprite> right2ImageList = new List<Sprite>();
+    public List<Sprite> right3ImageList = new List<Sprite>();
 
-    public Dictionary<string, Sprite> mainImages, leftImages, rightImages;
+    public Dictionary<string, Sprite> mainImages, leftImages, rightImages, left2images, right2images, left3images, right3images;
 
 	// Use this for initialization
 	void Start () {
@@ -17,7 +21,7 @@ public class ContentLoader : MonoBehaviour {
                 { "Kanjilicious", mainImageList[0] },
                 {"Cisco", mainImageList[1] },
                 { "Toma Leche", mainImageList[2] },
-                { "Fat Princess", mainImageList[3] },
+                { "Mitsubishi", mainImageList[3] },
                 { "BMW", mainImageList[4] },
                 { "Boardwalk", mainImageList[5] }
             };
@@ -27,9 +31,29 @@ public class ContentLoader : MonoBehaviour {
                 { "Kanjilicious", leftImageList[0] },
                 {"Cisco", leftImageList[1] },
                 { "Toma Leche", leftImageList[2] },
-                { "Fat Princess", leftImageList[3] },
+                { "Mitsubishi", leftImageList[3] },
                 { "BMW", leftImageList[4] },
                 { "Boardwalk", leftImageList[5] }
+            };
+
+        left2images = new Dictionary<string, Sprite>
+            {
+                { "Kanjilicious", left2ImageList[0] },
+                { "Cisco", left2ImageList[1] },
+                { "Toma Leche", left2ImageList[2] },
+                { "Mitsubishi", left2ImageList[3] },
+                { "BMW", left2ImageList[4] },
+                { "Boardwalk", left2ImageList[5] }
+            };
+
+        left3images = new Dictionary<string, Sprite>
+            {
+                { "Kanjilicious", left3ImageList[0] },
+                { "Cisco", left3ImageList[1] },
+                { "Toma Leche", left3ImageList[2] },
+                { "Mitsubishi", left3ImageList[3] },
+                { "BMW", left3ImageList[4] },
+                { "Boardwalk", left3ImageList[5] }
             };
 
         rightImages = new Dictionary<string, Sprite>
@@ -37,9 +61,29 @@ public class ContentLoader : MonoBehaviour {
                 { "Kanjilicious", rightImageList[0] },
                 {"Cisco", rightImageList[1] },
                 { "Toma Leche", rightImageList[2] },
-                { "Fat Princess", rightImageList[3] },
+                { "Mitsubishi", rightImageList[3] },
                 { "BMW", rightImageList[4] },
                 { "Boardwalk", rightImageList[5] }
+            };
+
+        right2images = new Dictionary<string, Sprite>
+            {
+                { "Kanjilicious", right2ImageList[0] },
+                {"Cisco", right2ImageList[1] },
+                { "Toma Leche", right2ImageList[2] },
+                { "Mitsubishi", right2ImageList[3] },
+                { "BMW", right2ImageList[4] },
+                { "Boardwalk", right2ImageList[5] }
+            };
+
+        right3images = new Dictionary<string, Sprite>
+            {
+                { "Kanjilicious", right3ImageList[0] },
+                {"Cisco", right3ImageList[1] },
+                { "Toma Leche", right3ImageList[2] },
+                { "Mitsubishi", right3ImageList[3] },
+                { "BMW", right3ImageList[4] },
+                { "Boardwalk", right3ImageList[5] }
             };
     }
 	
--- Assets/Scripts/Main/Navigation.cs
+++ Assets/Scripts/Main/Navigation.cs
@@ -53,6 +53,8 @@ public class Navigation : MonoBehaviour {
         {
             child.gameObject.SetActive(true);
         }
+        Transform back = GameObject.Find("Back").transform;
+        back.GetChild(0).gameObject.SetActive(true);
     }
 
     void DisableMainDisplay()
@@ -62,6 +64,8 @@ public class Navigation : MonoBehaviour {
         {
             child.gameObject.SetActive(false);
         }
+        Transform back = GameObject.Find("Back").transform;
+        back.GetChild(0).gameObject.SetActive(false);
     }
 
     void DisablePanel(Transform t)
@@ -85,9 +89,20 @@ public class Navigation : MonoBehaviour {
         GameObject leftPanel = GameObject.Find("Main Display").transform.GetChild(1).GetChild(0).gameObject;
         leftPanel.GetComponent<Image>().overrideSprite = cl.leftImages[project];
 
+        GameObject left2Panel = GameObject.Find("Main Display").transform.GetChild(3).GetChild(0).gameObject;
+        left2Panel.GetComponent<Image>().overrideSprite = cl.left2images[project];
+
+        GameObject left3Panel = GameObject.Find("Main Display").transform.GetChild(5).GetChild(0).gameObject;
+        left3Panel.GetComponent<Image>().overrideSprite = cl.left3images[project];
+
         GameObject rightPanel = GameObject.Find("Main Display").transform.GetChild(2).GetChild(0).gameObject;
         rightPanel.GetComponent<Image>().overrideSprite = cl.rightImages[project];
 
+        GameObject right2Panel = GameObject.Find("Main Display").transform.GetChild(4).GetChild(0).gameObject;
+        right2Panel.GetComponent<Image>().overrideSprite = cl.right2images[project];
+
+        GameObject right3Panel = GameObject.Find("Main Display").transform.GetChild(6).GetChild(0).gameObject;
+        right3Panel.GetComponent<Image>().overrideSprite = cl.right3images[project];
 
     }
 
