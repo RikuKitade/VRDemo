74c6ca2680fb1e4648b8154aa41ea232ab014597
timothsp <timothsp@uci.edu>
2017-02-09 16:30:26 -0800

    init

---
+++ Assets/Scripts/Main/Navigation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Navigation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void DisablePanels()
+    {
+        Transform panels = GameObject.Find("Panels").transform;
+        foreach (Transform col in panels)
+        {
+            foreach(Transform panel in col)
+            {
+                Image image = panel.GetComponent<Image>();
+                image.CrossFadeAlpha(0, 0.5f, false);
+            }
+        }
+    }
+}
